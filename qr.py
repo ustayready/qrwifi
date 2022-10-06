c57d8adb179d3c392bed225987de0e3bfdef3acd
ustayready <ustayready.web@fastmail.com>
2022-10-06 10:13:36 -0400

    Added Auth and Hidden Flags

--- qr.py
+++ qr.py
@@ -5,14 +5,22 @@ import png
 parser = argparse.ArgumentParser()
 parser.add_argument('--ssid', type=str, required=True)
 parser.add_argument('--password', type=str)
+parser.add_argument('--auth', type=str, default='WPA')
+parser.add_argument('--hidden', type=bool, default=False)
+
 args = parser.parse_args()
 
 def main(args):
-    generate(args.ssid, args.password)
+    generate(args.ssid, args.password, args.auth, args.hidden)
 
-def generate(ssid, password):
-    qr = pyqrcode.create(f'WIFI:T:WPA;S:{ssid};P:{password};;')
-    qr.png(f'{ssid}.png', scale=5)
+def generate(ssid, password, auth, hidden):
+    qr_text = f'WIFI:T:{auth};S:{ssid};P:{password};H:{hidden};;'
+    print(f'[*] Generated QR Code: {qr_text}')
+
+    qr = pyqrcode.create(qr_text)
+    qr_file = f'{ssid}.png'
+    qr.png(qr_file, scale=5)
+    print(f'[*] Saving QRCode: {qr_file}')
 
 if __name__ == '__main__':
     main(args)
